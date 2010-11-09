bbb4b5b3777af3a84b878de935cea3da59eb96d8
Kristoffer Berdal <web@flexd.net>
2010-11-09 10:25:01 +0100

    Forgot validations

--- lib/replay.rb
+++ lib/replay.rb
@@ -4,6 +4,8 @@ class Replay
   mount_uploader :original, OriginalUploader
   mount_uploader :replay, ReplayUploader
   
+  validates_presence_of :description
+  validates_presence_of :original
   def async_parse
     Resque.enqueue(ParseReplay, self.id) 
   end
